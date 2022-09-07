1fc9a7713d2c40e0aa6b13a079b7c4826bbd877b
Polina <grigoryanpalina@gmail.com>
2022-09-07 22:19:32 +0400

    1

---
+++ src/Main.java
@@ -0,0 +1,34 @@
+public class Main {
+    public static void main(String[] args) {
+        //type varName = value;
+
+        byte b = 127;
+        short s = 129;
+        int i = 2000000;
+        long l = 9233372036854789l;
+        float f;
+        f = 12.5f;
+        double d;
+        d = 148.5;
+        char ch1, ch2;
+        ch1 = 'Y';
+        ch2 = 'U';
+        boolean bo;
+        bo = false;
+        bo = true;
+ //comment
+
+
+        System.out.println("b is " + b);
+        System.out.println("s is " + s);
+        System.out.println("i is " + i);
+        System.out.println("l is " + l);
+        System.out.println("f is " + f);
+        System.out.println("l is " + d);
+        System.out.println("ch1 is: " + ch1);
+        System.out.println("ch2 is: " + ch2);
+        System.out.println("bo is: " + bo);
+        System.out.println("bo is: " + bo);
+
+    }
+}
